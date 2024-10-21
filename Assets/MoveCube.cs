06c4efaed9da6ad1f840d42fd30f4fc9d1a2d045
Josef <66090566+Josefhz@users.noreply.github.com>
2024-10-21 16:25:40 -0300

    commit inicial

---
+++ Assets/MoveCube.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    public bool jogadorClicou;
+
+    public float velocidade;
+
+    public MeshRenderer meshRendererCubo;
+
+    public ParticleSystem ExplosaoPrefab;
+
+
+    private void Awake() // Primeiro método a ser chamado ao dar play. ** ANTES MESMO DO LOAD ** 
+    {
+        Debug.Log("Teste de chamada - função Awake");
+    }
+
+    private void OnEnable() // Segundo método a ser chamado, e sempre será chamado quando o objeto ficar ativo.
+    {
+        Debug.Log("Teste de chamada - função OnEnable");
+    }
+
+    private void OnDisable() // Sempre será chamada quando o objeto for desativado.
+    {
+        Debug.Log("Teste de chamada - função OnDisable");
+    }
+
+    private void Start() // Terceiro método a ser chamado ao dar play. ** DEPOIS DO LOAD ** 
+    {
+        Debug.Log("Teste de chamada - função Start");
+
+        jogadorClicou = false;
+
+        meshRendererCubo = GetComponent<MeshRenderer>();
+
+    }
+
+    private void Update() // método chamado TODO FRAME.
+    {
+        // Programa coisa como:
+        // Input do jogador
+        // Movimento continuo
+        // Contagem de tempo
+        // Ação em estado
+
+        if (jogadorClicou == true)
+        {
+            transform.position += new Vector3(1,0,0) * Time.deltaTime * velocidade;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            jogadorClicou = true;
+            Debug.Log("Teste de chamada - Update Botão esquerdo apertado.");
+        }
+
+        
+    }
+
+    private void FixedUpdate() // método chamado num tempo setado/fixado/unico de frames
+    {
+        // Debug.Log("Teste de chamada - FixedUpdate");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // abrir alguma coisa
+        // mudar alguma fase
+        // alterar alguma informacao
+        // 
+        Debug.Log("Teste de chamada - OnTriggerEnter");
+
+        meshRendererCubo.materials[0].color = Color.green;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Teste de chamada - OnCollisionEnter - objeto: " + collision.gameObject.name);
+
+        collision.gameObject.GetComponent<MeshRenderer>().materials[0].color = Color.red;
+
+        Destroy(this.gameObject);
+    }
+
+    private void OnDestroy() // Método chamado após a destruição de um objeto.
+    {
+        // chamar algum efeito pós destruição / pós morte
+
+        Debug.Log("Teste de chamada - OnDestroy");
+
+        Instantiate(ExplosaoPrefab, new Vector3(0, 2, 0), ExplosaoPrefab.transform.rotation);
+    }
+
+    private void OnMouseEnter() // método chamado após o mouse entrar em contato com certa UI
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    private Rigidbody rb;
+
+
+    private void Awake() // Primeiro método a ser chamado antes do LOADING da scene
+    {
+        Debug.Log("Awake Call");
+
+        rb = GetComponent<Rigidbody>();
+
+        Debug.Log("Rigidbody atribuído com sucesso");
+    }
+
+    private void Start() // Segundo método a ser chamado, depois do LOADING
+    {
+        Debug.Log("Start Call");
+    }
+
+    private void Update() // Método que é chamado TODO Frame
+    {
+        // Inputs
+        // Lógicas
+        // Movimentos
+        // Estados
+        // Ações
+
+        if (Input.GetMouseButtonDown(0)) // 0 == Botão esquerdo 1 == Botão direito
+        {
+            Debug.Log("Botão esquerdo apertado.");
+
+            rb.isKinematic = false;
+        }
+     
+    }
+
+    private void OnTriggerEnter(Collider other) // método chamado quando ESSE GameObject tocar um Colisor Trigger
+    {
+        Debug.Log("OnTriggerEnter Call");
+
+        gameObject.GetComponent<MeshRenderer>().materials[0].color = Color.red;
+    }
+
+    private void OnCollisionEnter(Collision collision) // método chamado quando ESSE GameObject tocar um Colisor
+    {
+        Debug.Log("OnCollisionEnter Call");
+
+        collision.gameObject.GetComponent<MeshRenderer>().materials[0].color = Color.red;
+
+        Destroy(this.gameObject);
+    }
+
+    private void OnDestroy() // método chamado ao destruir esse GameObject
+    {
+        Debug.Log("OnDestroy Call");
+
+        // Explosoes
+        // Efeitos
+        // Mudanças
+        // Animações
+
+    }
+}
