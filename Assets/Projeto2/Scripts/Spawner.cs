5ba03346a83475db43d04d284a2eca119d8e3bf7
Josef <66090566+Josefhz@users.noreply.github.com>
2024-11-13 17:35:23 -0300

    commit

---
+++ Assets/Projeto2/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject MonsterPrefab;
+    
+    void Start()
+    {
+        SpawnMonsters();
+    }
+
+    private void SpawnMonsters()
+    {
+        Instantiate(MonsterPrefab, this.transform.position, Quaternion.identity);
+        // essa função, instancia um Prefab do Monstro, nessa posição, e na rotação padrão.
+    }
+}
